a5946696e51c5d782071012b011086cd7aa83d11
Allan Wirth <allanlw@gmail.com>
2019-10-22 17:06:00 -0400

    Add different item types

--- src/index.js
+++ src/index.js
@@ -1,42 +1,76 @@
 import m from "mithril";
 
 import "./style.scss";
-
 import BinaryHeap from "./binaryheap.js";
 
-var promiseAccept = null;
-
-function sort(text, progress) {
-  let items = text.split("\n").filter(function(x) { return x.length > 0; });
-  return BinaryHeap.sortTopN(items, cmpFunc, progress, items.length);
-}
+var ImgItem = {
+  slug: "img",
+  description: "Image URLs",
+  view: function(vnode) {
+    return <img src={vnode.attrs.item} id={vnode.attrs.id}/>
+  },
+};
+var IframeItem = {
+  slug: "iframe",
+  description: "Web pages",
+  view: function(vnode) {
+    return <iframe src={vnode.attrs.item} id={vnode.attrs.id}/>
+  },
+};
+var TextItem = {
+  slug: "text",
+  description: "Plain Text",
+  view: function(vnode) {
+    return <span id={vnode.attrs.id}>{vnode.attrs.item}</span>;
+  },
+};
 
 var App = {
+  promiseAccept: null,
   oninit: function(vnode) {
     this.mode = "start";
+    this.itemType = null;
+    this.comparison = null;
   },
   view: function(vnode) {
     switch (this.mode) {
     case "start":
-      return <>
+      let itemTypes = [
+        ImgItem,
+        IframeItem,
+        TextItem,
+      ];
+
+      let submit = e => {
+        this.mode = "sort";
+        let text = document.getElementById("entry").value;
+        let typeSlug = document.querySelector("input[name=\"item_type\"]:checked").value;
+        this.itemType = itemTypes.filter(x => x.slug == typeSlug)[0];
+        m.redraw.sync(); // force creation of the frames
+        this.sort(text).then(result => {
+          this.result = result;
+          this.mode = "done";
+        });
+      };
+
+      return <form id="start">
         <label for="entry">Enter URLs separated by newlines:</label>
         <textarea id="entry"/>
-        <input type="button" value="Start" onclick={e => {
-          this.mode = "sort";
-          let text = document.getElementById("entry").value;
-          m.redraw.sync(); // force creation of the frames
-          sort(text, App.progress.bind(this)).then(result => {
-            this.result = result;
-            this.mode = "done";
-          });
-        }}/>
-      </>;
+        {itemTypes.map((x,i) => {
+          return <>
+            <label for="item_type_{x.slug}">{x.description}</label>
+            <input type="radio" name="item_type" value={x.slug} checked={i==0}/>
+          </>
+        })}
+        <input type="button" value="Start" onclick={submit}/>
+      </form>;
     case "sort":
+      if (this.comparison == null) return null;
       return <>
         <span id="info">{JSON.stringify(this)}</span>
         <div id="container">
-          <iframe id="a"/>
-          <iframe id="b"/>
+          {m(this.itemType, {item: this.comparison[0], id:"a"})}
+          {m(this.itemType, {item: this.comparison[1], id:"b"})}
         </div>
       </>;
     case "done":
@@ -48,32 +82,29 @@ var App = {
     this.done = done;
     this.total = total;
     this.heapSize = heapSize;
-  }
+  },
+  cmp: function(a, b) {
+    this.comparison = [a,b];
+    m.redraw();
+    return new Promise(function(accept) {
+      App.promiseAccept = accept;
+    });
+  },
+  sort: function(text) {
+    let items = text.split("\n").filter(function(x) { return x.length > 0; });
+    return BinaryHeap.sortTopN(items, this.cmp.bind(this), this.progress.bind(this), items.length);
+  },
+  keypress: function(e) {
+    if (App.promiseAccept == null) return;
+    if (e.key == "a") {
+      App.promiseAccept(-1);
+    } else if (e.key == "f") {
+      App.promiseAccept(1);
+    }
+    m.redraw();
+  },
 };
 
 m.mount(document.body, App);
 
-// Returns -1 if a is a better than b, else returns 1
-function cmpFunc(a, b) {
-  var x = document.getElementById("a");
-  var y = document.getElementById("b");
-
-  x.src = a;
-  y.src = b;
-
-  console.log("cmp2 " + a + " , "+ b);
-
-  return new Promise(function(accept) {
-    promiseAccept = accept;
-  });
-}
-
-document.addEventListener("keypress", function(e) {
-  if (promiseAccept == null) return;
-  if (e.key == "a") {
-    promiseAccept(-1);
-  } else if (e.key == "f") {
-    promiseAccept(1);
-  }
-  m.redraw();
-});
+document.addEventListener("keypress", App.keypress);
